0109d76ad169e5316ee5a7d94e9f15c95d8fde43
AbhayGupta01 <abhaygupta083@gmail.com>
2023-07-20 21:58:19 +0530

    initial commit

---
+++ client/build/precache-manifest.fcb8021bbb68372e1c7f9c0577657eb9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e508d9d92b83a3ed0c6beb7523b102",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40e6531a823ac2948362",
+    "url": "/static/css/main.b12393ef.chunk.css"
+  },
+  {
+    "revision": "7a3f005bbcc40db0152d",
+    "url": "/static/js/2.e1857ae7.chunk.js"
+  },
+  {
+    "revision": "d8caecf788a09bc2b875ee2fe3866740",
+    "url": "/static/js/2.e1857ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40e6531a823ac2948362",
+    "url": "/static/js/main.f512afea.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.f512afea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "52d1238c6501e41f4a48e062653cc179",
+    "url": "/static/media/text1.52d1238c.png"
+  }
+]);
