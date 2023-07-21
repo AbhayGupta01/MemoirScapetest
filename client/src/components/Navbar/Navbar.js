d0163a2a40aba88e1daa793c46e19f95d390bff2
AbhayGupta01 <abhaygupta083@gmail.com>
2023-07-21 14:41:49 +0530

    deploy

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = ({change, setChange}) => {
     navigate('/');
     setUser(null);
     //addd this to reload react components
-    // setChange((prev)=> !prev )
+    setChange((prev)=> !prev )
   }
 
 
@@ -67,9 +67,9 @@ const Navbar = ({change, setChange}) => {
       <div className={classes.profile}>
         <Avatar
         className={classes.purple}
-        alt={user.result?.name}
-        src={user.result?.imageUrl} >{user.result?.name.charAt(0)}</Avatar>
-        <Typography className={classes.userName} variant='h6'>{user.result?.name}</Typography>
+        alt={user.result.name}
+        src={user.result.imageUrl} >{user.result.name.charAt(0)}</Avatar>
+        <Typography className={classes.userName} variant='h6'>{user.result.name}</Typography>
         <Button variant='contained' className={classes.logout} color='secondary' onClick={logout}>Logout</Button>
       </div>
     )
