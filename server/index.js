4a5d6717dec9c684376118d37882f81de196fea2
AbhayGupta01 <abhaygupta083@gmail.com>
2023-07-21 15:04:58 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -12,12 +12,19 @@ const app = express();
 dotenv.config();
 
 
+const PORT = process.env.PORT || 5000;
 //img size
 app.use(bodyParser.json({limit:"30mb",extended:true}))
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
 //cors need to be above routes
 app.use(cors())
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
 
+// app.listen(PORT, function () {
+//   console.log(`CORS-enabled web server listening on port ${PORT}`)
+// })
 
 //every route inside postRoutes is gonna start  with /posts
 app.use('/posts', postRoutes);
@@ -29,7 +36,6 @@ app.get('/', (req,res) => {
   res.send('Hello Memories Api, the backend is now running')
 });
 
-const PORT = process.env.PORT || 5000;
 
 //for errors
 mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
